70ed62754b0a2301a8edc51f85ec3a6a7fe139b9
liushuo <manunited_22@sina.com>
2017-01-04 15:32:18 +0800

    change index。js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var routes = require('./routes/index')
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 routes(app)
 
 app.listen(app.get('port'), function () {
-  consoleo.log('Express server listening on port ' + app.get('port'))
+  console.log('Express server listening on port ' + app.get('port'))
 })
 
 // catch 404 and forward to error handler
