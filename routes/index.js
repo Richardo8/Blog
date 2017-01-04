33d5aabed402f3f5f11c2c84d50b914e9cc0422e
liushuo <manunited_22@sina.com>
2017-01-04 17:08:21 +0800

    修改目录错误 添加用户注册功能

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var crypto = require('crypto'),
+    User = require('../modules/user.js');
+
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
@@ -14,6 +17,39 @@ module.exports = function (app) {
     res.render('reg', {title: '注册'})
   });
   app.post('/reg', function (req, res) {
+    var name = req.body.name,
+        password = req.body.password,
+        password_re = req.body['password-repeat'];
+    if(password_re != password){
+      req.flash('error', '两次输入的密码不一致！');
+      return res.redirect('/reg')
+    }
+    var md5 = crypto.createHash('md5'),
+        password = md5.update(req.body.password).digest('hex');
+    var newUser = new User({
+      name: name,
+      password: password,
+      email: req.body.email
+    });
+    User.get(newUser.name, function (err, user) {
+      if(err){
+        req.flash('error', err);
+        return res.redirect('/');
+      }
+      if(user){
+        req.flash('error', '用户已存在');
+        return res.redirect('/reg');
+      }
+      newUser.save(function (err, user) {
+        if(err){
+          req.flash('error', err);
+          return res.redirect('/reg');
+        }
+        req.session.user = user;
+        req.flash('success', '注册成功');
+        res.redirect('/')
+      })
+    })
   });
   app.get('/login', function (req, res) {
     res.render('login', {title: '登录'})
