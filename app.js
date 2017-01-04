0bb2233e707209465f1d6f459dea8216216c9f5a
liushuo <manunited_22@sina.com>
2017-01-04 15:27:08 +0800

    change app.js

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes/index')
 
 var app = express();
+
+app.set('port', process.env.PORT || 3000);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,12 +19,15 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+routes(app)
+
+app.listen(app.get('port'), function () {
+  consoleo.log('Express server listening on port ' + app.get('port'))
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
