254e02a71abe06b5d0852541b9e1c2ccf74f3938
liushuo <manunited_22@sina.com>
2017-01-06 15:05:51 +0800

    增加友链

--- routes/index.js
+++ routes/index.js
@@ -222,6 +222,15 @@ module.exports = function (app) {
     })
   });
 
+  app.get('/links', function (req, res) {
+    res.render('links', {
+      title: '友情链接',
+      user: req.session.user,
+      success: req.flash('success').toString(),
+      error: req.flash('error').toString()
+    })
+  })
+
   app.get('/search', function (req, res) {
     Post.search(req.query.keyword, function (err, posts) {
       if(err){
@@ -236,7 +245,7 @@ module.exports = function (app) {
         error: req.flash('error').toString()
       })
     })
-  })
+  });
 
   app.get('/u/:name', function (req, res) {
     var page = req.query.p ? parseInt(req.query.p) : 1;
